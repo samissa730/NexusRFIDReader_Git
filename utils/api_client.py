e52fe7ceb33d8e3ff6d676cfc0b1a6a9535f315f
samissa730 <sam.issa730@gmail.com>
2025-10-10 11:02:23 +0200

    Remove api temporally

--- utils/api_client.py
+++ utils/api_client.py
@@ -94,7 +94,8 @@ class ApiClient:
             data = response.json()
             return data.get('metadata', {}).get('code') == '200'
         except Exception:
-            logger.error("Uploading records failed")
+            # logger.error("Uploading records failed")
+            pass
         finally:
             http.close()
         return False
